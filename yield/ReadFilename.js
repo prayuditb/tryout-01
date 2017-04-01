7bfd0c3550963974e2cb9cd44e2c9e5de3acc269
Prayudi Tirta Bayu <prayuditb@gmail.com>
2017-04-01 11:30:13 +0700

    modify readfile.js

--- yield/ReadFilename.js
+++ yield/ReadFilename.js
@@ -1,15 +1,15 @@
-var testFolder = './../';
+var path = './../'; // change to your prefer directory
 var fs = require('fs');
 
 function *generator(){   
-    var file = fs.readdirSync(testFolder);
+    var file = fs.readdirSync(path);
     for(var i = 0; i < file.length; i++ ){
         yield file[i];
     }
 }
 
 var gen = generator();
-var list = fs.readdirSync(testFolder);
+var list = fs.readdirSync(path);
 for (var i = 0; i< list.length; i++){
     console.log(gen.next().value);
 }
