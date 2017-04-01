813554da76fe0da11a18c0e8ecef16a13fadb333
Prayudi Tirta Bayu <prayuditb@gmail.com>
2017-04-01 13:06:18 +0700

    delete unnecessary code

--- graphql/client/Client/app.js
+++ graphql/client/Client/app.js
@@ -44,14 +44,4 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
